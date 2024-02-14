50bec3e6c2945569f09539fc2163c6aa0c35fd28
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-14 12:02:05 -0500

    Booking form placeholder added

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -3,8 +3,11 @@ import { useState, useRef } from 'react';
 const Booking = () => {
     return (
         <section>
-            <h1>Booking</h1>
-            <iframe width="340" height="655" src="https://13b530f6.sibforms.com/serve/MUIFAK0KavdrcCudVNrn55nrBIm7SpYvhJXcneDymEHUBoox2aCDcDK2Dg8EwcGWz8K9R52L5EzLUWY8Qpg7ilOHlZanS6nxkDCOoA-sP8aIptpyVGu92qsHcNUK_BnQzl3n2xfCruSq6ahEN0VDvZ2B865UKJr1m8dGfbGKBBmM20g6U3GQKxdsVp1H1B5dm0q9c1fnCZsToHx0" frameBorder="0" scrolling="auto" allowFullScreen style={{display: "block", marginLeft: "auto", marginRight: "auto", maxWidth: "100%"}}></iframe>
+            <div className="booking">
+                <h2>Booking Form</h2>
+                <p>Insert custom form from 3rd party CRM</p>
+                <p>We'll create this together to make sure the form gathers the correct information and is saved properly to a service like Mailchimp, Behiiv, or Brevo so you can capture contact inforamtion and create a mailing list or newsletter</p>
+            </div>
         </section>
     )
 }
