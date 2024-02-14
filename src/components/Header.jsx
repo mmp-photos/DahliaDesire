f70010040e5f83a674c9ff2ee78e092b18efdb3e
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-14 13:55:54 -0500

    H1 head now updates for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Container,
     Row,
@@ -11,18 +11,31 @@ import flowerLogo from './../assets/images/flower_logo.svg';
 const Header = ({ name, setName }) => {
     const [ visibility, setVisibility ] = useState('invisible');
     const navigate = useNavigate();
-
     const navigateHome = () => {
         navigate('/');
         location.reload();
     };
+
+    const seth1Size = () => {
+        let windowWidth = window.screen.width;
+        console.log(windowWidth)
+        if(windowWidth > 400){
+            return
+        } else if (name === "Dahlia") {
+            return "2rem"
+        } else {
+            return "1.5rem"
+        }
+    };
+    let h1Size = seth1Size();
+
 
     return(
         <header>
             {name
                 ?   <header>
                     <img className="primary-logo" style={{cursor: "pointer", opacity: 1, transition: "opacity 3s"}}src={flowerLogo} alt="Dahlia Logo" onClick={navigateHome}/>
-                    <h1>{name} Desire</h1>
+                    <h1 style={{fontSize: h1Size}} >{name} Desire</h1>
                     <nav>
                         <ul>
                             <li><Link to={{
