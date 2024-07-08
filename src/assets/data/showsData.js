8b0ded5dff52ddcd75403b931250966ebe49ff34
Matthew Mayer <grafixgeek@yahoo.com>
2024-07-07 23:28:41 -0400

    New shows added

--- src/assets/data/showsData.js
+++ src/assets/data/showsData.js
@@ -1,69 +1,108 @@
 const showsData = [
     {
+      id: "24021401",
       date: "2024-02-14 20:00:00",
       name: "Hot Legs",
       venue: "Metro Indy",
       tickets: ""
     },
     {
+        id: "24011901",
         date: "2024-01-19 19:00:00",
         name: "The French Connection",
         venue: "White Rabbit Cabaret",
         tickets: "https://www.eventbrite.com/e/the-french-connection-tickets-780865508757"
     },
     {
+        id: "24022201",
         date: "2024-02-22 20:00:00",
         name: "Spicy Magic Comedy Night",
         venue: "White Rabbit Cabaret",
         tickets: "https://www.eventbrite.com/e/spicy-magic-comedy-nite-tickets-815908553447?aff=oddtdtcreator"
     },
     {
+        id: "24051701",
         date: "2024-05-17 00:00:00",
         name: "Shit Show",
         venue: "State Street Pub",
         tickets: ""
     },
     {
+        id: "24051801",
         date: "2024-05-18 00:00:00",
         name: "Strapped",
         venue: "Backdoor (Bloomington, IN)",
         tickets: "https://www.bckdoor.com/tickets"
     },
     {
+        id: "24053101",
         date: "2024-05-31 00:00:00",
         name: "White Rabbit Cabaret",
         venue: "White Rabbit",
         tickets: ""
     },
     {
+        id: "24060101",
         date: "2024-06-01 00:00:00",
         name: "White Rabbit Cabaret",
         venue: "White Rabbit",
         tickets: ""
     },
     {
+        id: "24060601",
         date: "2024-06-06 00:00:00",
         name: "Girl Pride Presents: A Sapphic Celebration",
         venue: "The Vogue",
         tickets: "https://thevogue.com/events/a-sapphic-celebration-feat-daya-june-6-2024/?fbclid=IwZXh0bgNhZW0CMTAAAR06aq6ECi6_nBxmXZ2OhFS1h1xZ9RvujOqeux1kHfNl48ZaN8vSRR68yJ8_aem_AdN75p5fJYq-VU1j3OOi3vLnLCDot4geKQtM7EbWRnvtaqXKWwqPB5hhPlymmzA3Nx7QGge4rjB0lwfKcrzh3FFf#buy"
     },
     {
+        id: "240060701",
         date: "2024-06-07 00:00:00",
         name: "Maurice Mantinis Happy Hour",
         venue: "White Rabbit",
         tickets: ""
     },
     {
+        id: "24061401",
         date: "2024-06-14 00:00:00",
         name: "BAF",
         venue: "Almost Famous",
         tickets: ""
     },
     {
+        id: "24062901",
         date: "2024-06-29 21:00:00",
         name: "Rendezvous",
         venue: "White Rabbit",
         tickets: "https://www.eventbrite.com/e/white-rabbit-presents-rendezvous-a-burlesque-and-drag-revue-tickets-915664175197?aff=oddtdtcreator"
+    },
+    {
+        id: "24071201",
+        date: "2024-07-12 21:00:00",
+        name: "Burlesque AF- Getaway",
+        venue: "Almost Famous",
+        tickets: "https://almostfamousindy.com/indianapolis-chatham-home-almost-famous-events"
+    },
+    {
+        id: "24071901",
+        date: "2024-07-19 21:00:00",
+        name: "Kinker",
+        venue: "White Rabbit",
+        tickets: "https://www.eventbrite.com/e/beelzebabe-presents-kinker-tickets-940602436207?aff=oddtdtcreator"
+    },
+    {
+        id: "24072001",
+        date: "2024-07-20 20:00:00",
+        name: "The Getaway",
+        venue: "The Upstairs Cabaret @ Capones Downtown Speakeasy",
+        tickets: "https://www.eventbrite.com/e/the-upstairs-cabaret-tickets-939095920177?aff=erelpanelorg"
+    },
+    {
+        id: "24072601",
+        date: "2024-07-26 20:00:00",
+        name: "Frankie's Backyard Burlesque",
+        venue: "The Backyard at Downtown Olly's",
+        tickets: "https://www.eventbrite.com/e/frankies-backyard-burlesque-tickets-892176021497?utm_experiment=test_share_listing&aff=ebdsshios"
     }
 ];
 
